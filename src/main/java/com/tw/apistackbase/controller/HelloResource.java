248656da1e67d744bb23af596b36e7bb327eff36
AhGang <474494594@qq.com>
2019-07-16 19:30:15 +0800

    commit Employees about get all Employess

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,27 +1,58 @@
 package com.tw.apistackbase.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employees")
 public class HelloResource {
-
-    private final Logger log = Logger.getLogger(this.getClass().getName());
-
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
+    List<Employee> employeeList = new ArrayList<>();
+//    private final Logger log = Logger.getLogger(this.getClass().getName());
+//
+//    @GetMapping(path = "/{userName}", produces = {"application/json"})
+//    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//
+//        return ResponseEntity.ok("Hello:" + userName);
+//    }
+    @GetMapping
+    public ResponseEntity getAll(){
+        employeeList.add(new Employee(1001,"Zhangsan",20,"male",6000));
+        return ResponseEntity.ok(employeeList);
     }
-
+//    @PostMapping
+//    public ResponseEntity createAEmployee(@RequestBody Employee employee){
+//        employeeList.add(employee);
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//    }
+//    @PutMapping
+//    public  ResponseEntity putAEmployee(@RequestBody Employee employee){
+//        // Employee employee1 = employeeList.stream().filter(e -> e.getId() == employee.getId()).collect(Collectors.toList());
+//        for(int i = 0 ; i < employeeList.size(); i ++){
+//            if(employeeList.get(i).getId() == employee.getId()){
+//                employeeList.get(i).setName(employee.getName());
+//                employeeList.get(i).setAge(employee.getAge());
+//                employeeList.get(i).setGender(employee.getGender());
+//
+//            }
+//        }
+//        return ResponseEntity.ok(employeeList);
+//    }
+//    @DeleteMapping(path = "/{id}")
+//    public ResponseEntity deleteAEmployee(@PathVariable int id){
+//        for(int i = 0 ; i < employeeList.size(); i ++){
+//            if(employeeList.get(i).getId() == id){
+//                employeeList.remove(employeeList.get(i));
+//            }
+//        }
+//        return ResponseEntity.ok(employeeList);
+//    }
 
 }
---
+++ src/test/java/com/tw/apistackbase/EmployeesTest.java
@@ -0,0 +1,68 @@
+package com.tw.apistackbase;
+
+import com.tw.apistackbase.controller.Employee;
+import org.json.JSONArray;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class EmployeesTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void should_get_employess_when_get_employees() throws Exception {
+		// this.mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("[{\"id\":0,\"name\":\"Zhangsan\",\"age\":0,\"gender\":\"male\",\"salary\":6000}]")));
+		final MvcResult mvcResult = this.mockMvc.perform(get("/employees")).andExpect(status().isOk()).andReturn();
+		JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
+		assertEquals(1001,jsonArray.getJSONObject(0).getInt("id"));
+		assertEquals(20,jsonArray.getJSONObject(0).getInt("age"));
+		assertEquals("Zhangsan",jsonArray.getJSONObject(0).getString("name"));
+		assertEquals("male",jsonArray.getJSONObject(0).getString("gender"));
+		assertEquals(6000,jsonArray.getJSONObject(0).getInt("salary"));
+	}
+//	@Test
+//	public void should_get_a_specific_employee_when_get_an_employee_id() throws Exception{
+//
+//	}
+//	@Test
+//	public void should_get_specific_page_employees_when_get_page_number_and_pageSize_number() throws Exception{
+//
+//	}
+//	@Test
+//	public void should_get_screen_all_male_employees_when_get_a_gender() throws Exception{
+//
+//	}
+//	@Test
+//	public void should_add_an_employee_when_post_a_employee() throws Exception{
+//
+//	}
+//	@Test
+//	public void should_update_an_employee_when_put_a_employeeID() throws Exception{
+//
+//	}
+//	@Test
+//	public void should_delete_an_employee_when_delete_a_employeeID() throws Exception{
+//
+//	}
+}
