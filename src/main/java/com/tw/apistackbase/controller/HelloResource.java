e0916e287fb59d2ce2633446e1868f9bc429af44
AhGang <474494594@qq.com>
2019-07-16 21:05:06 +0800

    commit Employees about  should_add_an_employee_when_post_a_employee()  should_update_an_employee_when_put_a_employeeID()

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -17,7 +17,8 @@ import java.util.logging.Logger;
 public class HelloResource {
     Employee employeeA = new Employee(1001,"Zhangyi",20,"male",6000);
     Employee employeeB = new Employee(1002,"Zhanger",20,"female",7000);
-    List<Employee> employeeList = new ArrayList<>(Arrays.asList(employeeA,employeeB));
+    Employee employeeC = new Employee(1003,"Zhangsan",21,"male",7000);
+    List<Employee> employeeList = new ArrayList<>(Arrays.asList(employeeA,employeeB,employeeC));
 //    private final Logger log = Logger.getLogger(this.getClass().getName());
 //
 //    @GetMapping(path = "/{userName}", produces = {"application/json"})
@@ -48,27 +49,29 @@ public class HelloResource {
         }
         return ResponseEntity.ok().build();
     }
-//    @PutMapping
-//    public  ResponseEntity putAEmployee(@RequestBody Employee employee){
-//        // Employee employee1 = employeeList.stream().filter(e -> e.getId() == employee.getId()).collect(Collectors.toList());
-//        for(int i = 0 ; i < employeeList.size(); i ++){
-//            if(employeeList.get(i).getId() == employee.getId()){
-//                employeeList.get(i).setName(employee.getName());
-//                employeeList.get(i).setAge(employee.getAge());
-//                employeeList.get(i).setGender(employee.getGender());
-//
-//            }
-//        }
-//        return ResponseEntity.ok(employeeList);
-//    }
-//    @DeleteMapping(path = "/{id}")
-//    public ResponseEntity deleteAEmployee(@PathVariable int id){
-//        for(int i = 0 ; i < employeeList.size(); i ++){
-//            if(employeeList.get(i).getId() == id){
-//                employeeList.remove(employeeList.get(i));
-//            }
-//        }
-//        return ResponseEntity.ok(employeeList);
-//    }
+    @PostMapping
+    public ResponseEntity createAEmployee(@RequestBody Employee employee){
+        employeeList.add(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(employee);
+    }
+    @PutMapping(path = "/{id}")
+    public  ResponseEntity put(@RequestBody Employee employee){
+        // Employee employee1 = employeeList.stream().filter(e -> e.getId() == employee.getId()).collect(Collectors.toList());
+        for(int i = 0 ; i < employeeList.size(); i ++){
+            if(employeeList.get(i).getId() == employee.getId()){
+                employeeList.get(i).setAge(25);
+            }
+        }
+        return ResponseEntity.ok(employeeList);
+    }
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity deleteAEmployee(@PathVariable int id){
+        for(int i = 0 ; i < employeeList.size(); i ++){
+            if(employeeList.get(i).getId() == id){
+                employeeList.remove(employeeList.get(i));
+            }
+        }
+        return ResponseEntity.ok(employeeList);
+    }
 
 }
--- src/test/java/com/tw/apistackbase/EmployeesTest.java
+++ src/test/java/com/tw/apistackbase/EmployeesTest.java
@@ -2,13 +2,17 @@ package com.tw.apistackbase;
 
 import com.tw.apistackbase.controller.Employee;
 import org.json.JSONArray;
+
 import org.json.JSONObject;
+import org.json.JSONString;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,7 +21,7 @@ import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBui
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,48 +40,85 @@ public class EmployeesTest {
 		// this.mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("[{\"id\":0,\"name\":\"Zhangsan\",\"age\":0,\"gender\":\"male\",\"salary\":6000}]")));
 		final MvcResult mvcResult = this.mockMvc.perform(get("/employees")).andExpect(status().isOk()).andReturn();
 		JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
-		assertEquals(1001,jsonArray.getJSONObject(0).getInt("id"));
-		assertEquals(20,jsonArray.getJSONObject(0).getInt("age"));
-		assertEquals("Zhangyi",jsonArray.getJSONObject(0).getString("name"));
-		assertEquals("male",jsonArray.getJSONObject(0).getString("gender"));
-		assertEquals(6000,jsonArray.getJSONObject(0).getInt("salary"));
+		assertEquals(1001, jsonArray.getJSONObject(0).getInt("id"));
+		assertEquals(20, jsonArray.getJSONObject(0).getInt("age"));
+		assertEquals("Zhangyi", jsonArray.getJSONObject(0).getString("name"));
+		assertEquals("male", jsonArray.getJSONObject(0).getString("gender"));
+		assertEquals(6000, jsonArray.getJSONObject(0).getInt("salary"));
 	}
+
 	@Test
-	public void should_get_a_specific_employee_when_get_an_employee_id() throws Exception{
+	public void should_get_a_specific_employee_when_get_an_employee_id() throws Exception {
 		final MvcResult mvcResult = this.mockMvc.perform(get("/employees/1002")).andExpect(status().isOk()).andReturn();
 		JSONObject jsonObject = new JSONObject(mvcResult.getResponse().getContentAsString());
-		assertEquals(1002,jsonObject.getInt("id"));
-		assertEquals(20,jsonObject.getInt("age"));
-		assertEquals("Zhanger",jsonObject.getString("name"));
-		assertEquals("female",jsonObject.getString("gender"));
-		assertEquals(7000,jsonObject.getInt("salary"));
+		assertEquals(1002, jsonObject.getInt("id"));
+		assertEquals(20, jsonObject.getInt("age"));
+		assertEquals("Zhanger", jsonObject.getString("name"));
+		assertEquals("female", jsonObject.getString("gender"));
+		assertEquals(7000, jsonObject.getInt("salary"));
 
 	}
-//	@Test
+
+	//	@Test
 //	public void should_get_specific_page_employees_when_get_page_number_and_pageSize_number() throws Exception{
 //
 //	}
 	@Test
-	public void should_get_screen_all_male_employees_when_get_a_gender() throws Exception{
+	public void should_get_screen_all_male_employees_when_get_a_gender() throws Exception {
 		final MvcResult mvcResult = this.mockMvc.perform(get("/employees/?gender=male")).andExpect(status().isOk()).andReturn();
 		JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
-		assertEquals(1001,jsonArray.getJSONObject(0).getInt("id"));
-		assertEquals(20,jsonArray.getJSONObject(0).getInt("age"));
-		assertEquals("Zhangyi",jsonArray.getJSONObject(0).getString("name"));
-		assertEquals("male",jsonArray.getJSONObject(0).getString("gender"));
-		assertEquals(6000,jsonArray.getJSONObject(0).getInt("salary"));
+		assertEquals(1001, jsonArray.getJSONObject(0).getInt("id"));
+		assertEquals(20, jsonArray.getJSONObject(0).getInt("age"));
+		assertEquals("Zhangyi", jsonArray.getJSONObject(0).getString("name"));
+		assertEquals("male", jsonArray.getJSONObject(0).getString("gender"));
+		assertEquals(6000, jsonArray.getJSONObject(0).getInt("salary"));
 
 	}
-//	@Test
-//	public void should_add_an_employee_when_post_a_employee() throws Exception{
-//
-//	}
-//	@Test
-//	public void should_update_an_employee_when_put_a_employeeID() throws Exception{
-//
-//	}
-//	@Test
-//	public void should_delete_an_employee_when_delete_a_employeeID() throws Exception{
-//
-//	}
+
+	@Test
+	public void should_add_an_employee_when_post_a_employee() throws Exception {
+		Employee employeeC = new Employee(1001, "Zhangyi", 25, "male", 15000);
+		JSONObject employeeCJsonObject = new JSONObject(employeeC);
+		final MvcResult mvcResult = this.mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_PROBLEM_JSON_UTF8).content(employeeCJsonObject.toString())).andExpect(status().isCreated()).andReturn();
+		JSONObject jsonObject = new JSONObject(mvcResult.getResponse().getContentAsString());
+		assertEquals(1001, jsonObject.getInt("id"));
+		assertEquals(25, jsonObject.getInt("age"));
+		assertEquals("Zhangyi", jsonObject.getString("name"));
+		assertEquals("male", jsonObject.getString("gender"));
+		assertEquals(15000, jsonObject.getInt("salary"));
+
+	}
+
+	@Test
+	public void should_update_an_employee_when_put_a_employeeID() throws Exception {
+		Employee employeeC = new Employee(1001, "Zhangyi", 25, "male", 15000);
+		JSONObject employeeCJsonObject = new JSONObject(employeeC);
+		final MvcResult mvcResult = this.mockMvc.perform(put("/employees/1002").contentType(MediaType.APPLICATION_PROBLEM_JSON_UTF8).content(employeeCJsonObject.toString())).andExpect(status().isOk()).andReturn();
+		JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
+		assertEquals(1001, jsonArray.getJSONObject(0).getInt("id"));
+		assertEquals(25, jsonArray.getJSONObject(0).getInt("age"));
+		assertEquals("Zhangyi", jsonArray.getJSONObject(0).getString("name"));
+		assertEquals("male", jsonArray.getJSONObject(0).getString("gender"));
+		assertEquals(6000, jsonArray.getJSONObject(0).getInt("salary"));
+
+	}
+
+	@Test
+	public void should_delete_an_employee_when_delete_a_employeeID() throws Exception {
+		final MvcResult mvcResult = this.mockMvc.perform(delete("/employees/1002")).andExpect(status().isOk()).andReturn();
+		JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
+
+		assertEquals(1001, jsonArray.getJSONObject(0).getInt("id"));
+		assertEquals(20, jsonArray.getJSONObject(0).getInt("age"));
+		assertEquals("Zhangyi", jsonArray.getJSONObject(0).getString("name"));
+		assertEquals("male", jsonArray.getJSONObject(0).getString("gender"));
+		assertEquals(6000, jsonArray.getJSONObject(0).getInt("salary"));
+
+		assertEquals(1003, jsonArray.getJSONObject(1).getInt("id"));
+		assertEquals(21, jsonArray.getJSONObject(1).getInt("age"));
+		assertEquals("Zhangsan", jsonArray.getJSONObject(1).getString("name"));
+		assertEquals("male", jsonArray.getJSONObject(1).getString("gender"));
+		assertEquals(7000, jsonArray.getJSONObject(1).getInt("salary"));
+	}
 }
+
